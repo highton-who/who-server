75b529e5e7158db044c4a02207819bf4f8b556cc
root <1210thatman@gmail.com>
2025-10-13 16:58:47 +0900

    feat: controller 작성

--- src/main/java/com/pick/zick/domain/student/controller/StudentController.java
+++ src/main/java/com/pick/zick/domain/student/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.pick.zick.domain.student.controller;
 
 import com.pick.zick.domain.student.dto.request.CheckCanEnterRequest;
+import com.pick.zick.domain.student.dto.request.UpdateStudentRequest;
 import com.pick.zick.domain.student.dto.response.CheckCanEnterResponse;
 import com.pick.zick.domain.student.dto.response.ExcelResponse;
 import com.pick.zick.domain.student.service.AttendanceExcelService;
 import com.pick.zick.domain.student.service.CheckCanEnterService;
 import com.pick.zick.domain.student.service.GetRandomHash;
+import com.pick.zick.domain.student.service.UpdateStudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ public class StudentController {
     private final GetRandomHash getRandomHash;
     private final CheckCanEnterService checkCanEnterService;
     private final AttendanceExcelService attendanceExcelService;
+    private final UpdateStudentService updateStudentService;
 
     @GetMapping("/qr")
     public String getRandomHash() throws NoSuchAlgorithmException {
@@ -32,5 +35,10 @@ public class StudentController {
     @GetMapping("/attendances/excel")
     public ExcelResponse getAttendanceExcel() throws IOException {
         return attendanceExcelService.generateExcel();
+    }
+
+    @PatchMapping("students/{id}")
+    public void updateStudent(@PathVariable Long id, @RequestBody UpdateStudentRequest updateStudentRequest) {
+        updateStudentService.execute(id, updateStudentRequest);
     }
 }
