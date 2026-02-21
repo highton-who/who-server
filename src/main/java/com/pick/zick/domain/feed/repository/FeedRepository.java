af83e7abc5c4291fa5d8f3993533e048002ebad9
그분 <1210thatman@gmail.com>
2026-02-22 05:19:20 +0900

    feat: help me...

--- src/main/java/com/pick/zick/domain/feed/repository/FeedRepository.java
+++ src/main/java/com/pick/zick/domain/feed/repository/FeedRepository.java
@@ -1,7 +1,11 @@
 package com.pick.zick.domain.feed.repository;
 
 import com.pick.zick.domain.feed.entity.Feed;
+import com.pick.zick.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    List<Feed> findByUser(User user);
 }
--- src/main/java/com/pick/zick/domain/feed/service/GetFeedListService.java
+++ src/main/java/com/pick/zick/domain/feed/service/GetFeedListService.java
@@ -2,21 +2,27 @@ package com.pick.zick.domain.feed.service;
 
 import com.pick.zick.domain.feed.persistence.dto.response.FeedResponse;
 import com.pick.zick.domain.feed.repository.FeedRepository;
+import com.pick.zick.domain.user.entity.User;
+import com.pick.zick.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GetFeedListService {
     private final FeedRepository feedRepository;
+    private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
     public List<FeedResponse> getFeedList() {
-        return feedRepository.findAll().stream()
+        User user = userFacade.getCurrentUser();
+        return feedRepository.findByUser(user).stream()
                 .map(FeedResponse::from)
                 .collect(Collectors.toList());
     }
